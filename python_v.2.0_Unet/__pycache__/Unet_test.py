5a43ac980d83c2ae394159333f8b4e67ec519b58
ralfcat <37572934+ralfcat@users.noreply.github.com>
2024-06-14 16:49:21 +0200

    update

--- python_v.2.0_Unet/__pycache__/Unet_test.py
+++ python_v.2.0_Unet/__pycache__/Unet_test.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import KFold
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.layers import Dense, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
 import sequence_unet.models as models
-from tensorflow.keras.callbacks import EarlyStopping
 import keras
-
+import random
 print(tf.__version__)
 print(keras.__version__)
 
+#random.seed(0)
 #####################     TRAIN AND FINE TUNE THE SEQUENCE UNET MODEL (Patho_finetune)    ##################################
 #####################                                                                     ##################################
 
@@ -77,6 +79,9 @@ test_dataset = tf.data.Dataset.from_tensor_slices((padded_X_test_encoded, y_test
 test2_dataset = tf.data.Dataset.from_tensor_slices((padded_X_test2_encoded, y_test2)).batch(32)
 
 # Define the fine-tuned model
+
+## THIS WORKS !!!! 
+"""
 class FineTunedSequenceUNET(Model):
     def __init__(self, base_model, num_classes, dropout_rate=0.5):
         super(FineTunedSequenceUNET, self).__init__()
@@ -90,7 +95,44 @@ class FineTunedSequenceUNET(Model):
         x = self.flatten(x)
         x = self.dropout(x, training=training)  # Apply dropout only during training
         return self.dense(x)
+"""
+# TRYING TO ADD MORE LAYERS
+class FineTunedSequenceUNET(Model):
+    def __init__(self, base_model, num_classes, dropout_rate=0.5):
+        super(FineTunedSequenceUNET, self).__init__()
+        self.base_model = base_model
+        self.flatten = Flatten()
+        self.dense1 = Dense(256, activation='relu', kernel_regularizer=l2(0.01))
+        self.bn1 = BatchNormalization()
+        self.dropout1 = Dropout(dropout_rate)
+        self.dense2 = Dense(128, activation='relu', kernel_regularizer=l2(0.01))
+        self.bn2 = BatchNormalization()
+        self.dropout2 = Dropout(dropout_rate)
+        self.dense3 = Dense(64, activation='relu', kernel_regularizer=l2(0.01))
+        self.bn3 = BatchNormalization()
+        self.dropout3 = Dropout(dropout_rate)
+        self.output_dense = Dense(num_classes, activation='sigmoid')
 
+    def call(self, inputs, training=False):
+        x = self.base_model(inputs)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.bn1(x, training=training)
+        x = self.dropout1(x, training=training)
+        x = self.dense2(x)
+        x = self.bn2(x, training=training)
+        x = self.dropout2(x, training=training)
+        x = self.dense3(x)
+        x = self.bn3(x, training=training)
+        x = self.dropout3(x, training=training)
+        return self.output_dense(x)
+
+# Learning rate scheduler function
+def scheduler(epoch, lr):
+    if epoch < 10:
+        return lr
+    else:
+        return lr * tf.math.exp(-0.1)
 # Instantiate the fine-tuned model
 num_classes = 1  # Binary classification (pathogenic or not)
 
@@ -117,6 +159,26 @@ for fold, (train_index, val_index) in enumerate(kf.split(X_values), 1):
     val_loss, val_accuracy = fold_model.evaluate(val_dataset_fold)
     print(f'Fold {fold} - Validation Loss: {val_loss}, Validation Accuracy: {val_accuracy}')
 """
+for fold, (train_index, val_index) in enumerate(kf.split(X_values), 1):
+    print(f"Processing fold {fold}...")
+    X_train_fold, X_val_fold = X_values[train_index], X_values[val_index]
+    y_train_fold, y_val_fold = y_values[train_index], y_values[val_index]
+
+    train_dataset_fold = tf.data.Dataset.from_tensor_slices((X_train_fold, y_train_fold)).batch(32)
+    val_dataset_fold = tf.data.Dataset.from_tensor_slices((X_val_fold, y_val_fold)).batch(32)
+
+    # Load the pre-trained model for each fold
+    base_model = models.load_trained_model(model='patho_finetune', download=True)
+    fold_model = FineTunedSequenceUNET(base_model, num_classes=num_classes, dropout_rate=0.5)
+    fold_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    lr_scheduler = LearningRateScheduler(scheduler)
+
+    fold_model.fit(train_dataset_fold, validation_data=val_dataset_fold, epochs=50, callbacks=[lr_scheduler])
+
+    val_loss, val_accuracy = fold_model.evaluate(val_dataset_fold)
+    print(f'Fold {fold} - Validation Loss: {val_loss}, Validation Accuracy: {val_accuracy}')
+
 # Reinitialize and train a final model on the full training set
 base_model = models.load_trained_model(model='patho_finetune', download=True)
 final_model = FineTunedSequenceUNET(base_model, num_classes=num_classes, dropout_rate=0.5)
@@ -124,15 +186,16 @@ final_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accu
 
 # Setup early stopping
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-final_model.fit(train_dataset, epochs=20, validation_data=test_dataset, callbacks=[early_stopping])
+lr_scheduler = LearningRateScheduler(scheduler)
+
+final_model.fit(train_dataset, epochs=50, validation_data=test_dataset, callbacks=[early_stopping, lr_scheduler])
 
 # Evaluate the final model on the test set
 test_loss, test_accuracy = final_model.evaluate(test_dataset)
 test_loss2, test_accuracy2 = final_model.evaluate(test2_dataset)
 print(f'Test Loss: {test_loss}, Test Accuracy: {test_accuracy}')
+print('SECOND TEST: \n')
 print(f'Test Loss: {test_loss2}, Test Accuracy: {test_accuracy2}')
-
-
 
 ##############  USE THE FINE-TUNED MODEL TO ASSIGN NEW LABELS TO DATA   #########################
 
@@ -157,6 +220,7 @@ predicted_labels = (predictions > 0.5).astype(int).flatten()
 new_data['Predicted_Pathogenicity'] = predicted_labels
 
 # Save the dataset with the new predictions
-new_data.to_csv('C:/Users/victo/Documents/GitHub/Interpretation_of_NN_using_RBM/Interpretation_of_NN_using_RBM/data/NS1/NS1_H5_H7_Train2_with_Predictions.csv', index=False)
+labled_data_path = "C:/Users/victo/Documents/GitHub/Interpretation_of_NN_using_RBM/Interpretation_of_NN_using_RBM/data/NS1/NS1_H5_H7_Train2_with_Predictions.csv"
+new_data.to_csv(labled_data_path, index=False)
 
 print("New labels assigned and saved successfully.")
